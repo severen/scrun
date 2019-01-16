f6548c220cec67bae091891680ba921931522e7e
Severen Redwood <severen@shrike.me>
2019-01-17 00:40:08 +0100

    Add argument parsing using Clap

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@
 
 use std::{boxed::Box, error::Error};
 
+use clap::{
+    App,
+    AppSettings,
+    Arg,
+    crate_name,
+    crate_description,
+    crate_version,
+    crate_authors,
+};
 use cursive::{Cursive, event::Key};
 
 mod view;
@@ -13,9 +22,18 @@ mod buffer;
 use crate::{view::EditorView, buffer::Buffer};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new(crate_name!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(Arg::with_name("FILE").help("The file to edit").index(1))
+        .get_matches();
+
     let mut siv = Cursive::default();
 
-    let buffer = Buffer::from_file("test/large.txt")?;
+    // It is safe to use unwrap here since the FILE parameter is required.
+    let buffer = Buffer::from_file(&matches.value_of("FILE").unwrap())?;
     siv.add_fullscreen_layer(EditorView::new(buffer));
     siv.add_global_callback(Key::End, |c| c.quit());
 
